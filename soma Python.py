8b6c9a2b5973721ab67e8902ac1399296d40e061
Roger Ramos <44578612+Rogergramos@users.noreply.github.com>
2021-12-04 17:20:59 -0300

    Add files via upload

---
+++ soma Python.py
@@ -0,0 +1,15 @@
+a = 10
+b = 20
+soma = a + b
+print(soma)
+
+print("---")
+
+dividir = soma/2
+print (dividir)
+print (soma/2)
+print ("Divisão ")
+string = "Divisão "
+print (string, dividir)
+print (f"variavel {a} + {b} / 2 = {dividir}")
+print ("variavel {a} + {b} / 2 = {dividir}")
